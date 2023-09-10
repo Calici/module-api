06f2847e4356f3267b3bccc8ac2b0e0906570130
Jonathan Willianto <jowillianto@gmail.com>
2023-09-10 16:22:06 +0900

    nightly build config

--- setup.py
+++ setup.py
@@ -13,7 +13,16 @@ setup(
       "module_api.common"
     ],
     install_requires=[
-        "pyyaml", 
-        "requests"
+      "annotated-types==0.5.0",
+      "certifi==2023.5.7",
+      "charset-normalizer==3.1.0",
+      "idna==3.4",
+      "pydantic==2.0", 
+      "pydantic_core==2.0.1",
+      "pytz==2023.3",
+      "PyYAML==6.0",
+      "requests==2.31.0",
+      "typing_extensions==4.7.1",
+      "urllib3==2.0.3",
     ]
 )
