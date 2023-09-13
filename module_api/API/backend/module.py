ba071d24554c6502d6a67a1c4c29724d7a63a616
Jonathan Willianto <jowillianto@gmail.com>
2023-09-14 02:35:44 +0900

    added share dir work dir switching to api

--- module_api/API/backend/module.py
+++ module_api/API/backend/module.py
@@ -11,6 +11,7 @@ from typing import \
   Type, \
   Dict, \
   Callable
+import shutil
 
 class ModuleStatus:
     """
@@ -30,6 +31,48 @@ class ModuleResult(lock.LockSection):
     def serialize_changes(self) -> dict:
         # Force Serialization:
         return self.serialize()
+    def convert_to_shared(self, 
+        work_dir : pathlib.Path, 
+        share_dir : pathlib.Path                      
+    ):
+        new_contents = {}
+        for field_name, field in self.items():
+            value = field.get()
+            if isinstance(value, pathlib.Path) and work_dir in value.parents:
+                new_contents[field_name] = \
+                    share_dir / value.relative_to(work_dir)
+        self.set_value(new_contents)
+    def convert_to_work(self, 
+        work_dir : pathlib.Path, 
+        share_dir : pathlib.Path
+    ):
+        self.convert_to_shared(share_dir, work_dir)
+    def create_symlnk(self, 
+        work_dir : pathlib.Path, share_dir : pathlib.Path                
+    ):
+        for field in self.values():
+            path = field.get()
+            if isinstance(path, pathlib.Path):
+                if share_dir in path.parents:
+                    share_path = path
+                    work_path = work_dir / path.relative_to(share_dir)
+                    self.__create_symlnk_ignore_errors(work_path, share_path)
+                elif work_dir in path.parents:
+                    share_path = share_dir / path.relative_to(work_dir)
+                    work_path = path
+                    self.__create_symlnk_ignore_errors(work_path, share_path)
+                    
+    def __create_symlnk_ignore_errors(
+        self, src : pathlib.Path, target : pathlib.Path
+    ):
+        if not target.parent.exists():
+            target.parent.mkdir(0o777, True, True)
+        if target.is_symlink(): target.unlink()
+        elif target.is_file(): target.unlink()
+        elif target.is_dir(): shutil.rmtree(target)
+        target.symlink_to(src)
+            
+    
     
 
 class ModuleSection(RequestAutoRefresh):
