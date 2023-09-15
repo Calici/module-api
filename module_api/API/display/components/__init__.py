eee764b0c5b905dbfc351f58de3b4c1af31ea8cc
Jonathan Willianto <jowillianto@gmail.com>
2023-09-15 11:13:34 +0900

    fixed __init__.py

--- module_api/API/display/components/__init__.py
+++ module_api/API/display/components/__init__.py
@@ -1,11 +1,45 @@
-from .v0_no_table import ComponentWithoutTable as v0_ComponentWithoutTable
-from .v0_table import ComponentWithTable as v0_ComponentWithTable
-from .v1_table import ComponentWithTable as v1_ComponentWithTable
-from .v1_no_table import ComponentWithoutTable as v1_ComponentWithoutTable
+from .v0_no_table import \
+  ComponentWithoutTable as v0_ComponentWithoutTable
+from .v0_table import \
+  ComponentWithTable as v0_ComponentWithTable
+from .v1_table import \
+  ComponentWithTable as v1_ComponentWithTable, \
+  Header as v1_Header, \
+  RowListField as v1_Rows, \
+  MutableTable as v1_MutableTable
+from .v1_no_table import \
+  ComponentWithoutTable as v1_ComponentWithoutTable
+from .common import \
+  v1_SmartBoxes, \
+  v1_Messages, \
+  v1_ProgressField, \
+  v1_ControlConfig, \
+  v1_TimeField, \
+  v1_SmartBox, \
+  v0_ControlConfig, \
+  v0_Messages, \
+  v0_MutableTable, \
+  v0_ProgressField
 
 __all__ = [
+  # Version 0
   'v0_ComponentWithoutTable', 
+  'v0_ComponentWithTable',
+  'v0_ControlConfig', 
+  'v0_Messages', 
+  'v0_MutableTable', 
+  'v0_ProgressField',
+
+  # Version 1
+  'v1_Header', 
+  'v1_Rows', 
+  'v1_MutableTable', 
+  'v1_SmartBoxes', 
+  'v1_Messages', 
+  'v1_ProgressField', 
+  'v1_ControlConfig', 
+  'v1_TimeField', 
+  'v1_SmartBox', 
   'v1_ComponentWithoutTable', 
-  'v1_ComponentWithTable', 
-  'v0_ComponentWithTable'
+  'v1_ComponentWithTable'
 ]
