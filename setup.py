ea7f161eb01f2e81d1659bb25c05b714edc87339
Jonathan Willianto <jowillianto@gmail.com>
2023-11-06 15:22:06 +0900

    downgraded dependencies for python3.7 compatibility

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
       f"module_api.{pkg_name}" for pkg_name in find_namespace_packages("module_api")
     ],
     install_requires=[
-      "annotated-types==0.6.0",
+      "annotated-types==0.5.0",
       "certifi==2023.7.22",
       "charset-normalizer==3.3.2",
       "idna==3.4",
@@ -24,7 +24,7 @@ setup(
       "pytz==2023.3.post1",
       "PyYAML==6.0.1",
       "requests==2.31.0",
-      "typing_extensions==4.8.0",
+      "typing_extensions==4.7.1",
       "urllib3==2.0.7", 
       "pypharmaco @ https://github.com/Calici/pypharmaco/releases/download/v0.0.1/pypharmaco-0.0.1-py3-none-any.whl"
     ]
