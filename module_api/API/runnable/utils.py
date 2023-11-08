8bf6b9b1ec35190e58828e7b875d324ee9282c97
Jonathan Willianto <jowillianto@gmail.com>
2023-11-08 14:36:50 +0900

    List -> Iterable

--- module_api/API/runnable/utils.py
+++ module_api/API/runnable/utils.py
@@ -1,9 +1,9 @@
 import pathlib
-from typing_extensions import List, Union
+from typing_extensions import Iterable, Union
 
 def create_directories(
     workdir : pathlib.Path, 
-    dir_list : List[Union[str, pathlib.Path]], 
+    dir_list : Iterable[Union[str, pathlib.Path]], 
     permission : int = 0o775
 ):
     for dir in dir_list:
