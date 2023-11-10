aebf7cf0de518df96328cf7eab09fd2bc9ef1e06
Jonathan Willianto <jowillianto@gmail.com>
2023-11-10 15:18:19 +0900

    added export

--- module_api/API/display/__init__.py
+++ module_api/API/display/__init__.py
@@ -3,12 +3,14 @@ from .components import \
   v0_ComponentWithoutTable, \
   v0_ComponentWithTable, \
   v1_ComponentWithoutTable, \
-  v1_ComponentWithTable
+  v1_ComponentWithTable, \
+  ComponentWithPDFViewer
 
 __all__ = [
   'Display', 
   'v0_ComponentWithoutTable', 
   'v0_ComponentWithTable',
   'v1_ComponentWithoutTable',
-  'v1_ComponentWithTable'
+  'v1_ComponentWithTable', 
+  'ComponentWithPDFViewer'
 ]
