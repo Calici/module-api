69d14cd8033a808755dca03b2de4133132d0e1b8
Jonathan Willianto <jowillianto@gmail.com>
2023-11-24 17:49:14 +0900

    [#4] Added type and upgraded to default to version 2

--- module_api/API/error/error.py
+++ module_api/API/error/error.py
@@ -1,16 +1,22 @@
 # Library Import
 import json
-from typing_extensions import IO
+from typing_extensions import IO, Literal
 
 # API Import
 import module_api.API.lock as lock
-from module_api.API.display.component import Message
 
+ErrorT = Literal["WARNING", "ERROR"]
+
+class ErrorMessage(lock.LockSection):
+    title = lock.LockField(type = str, default = '')
+    content = lock.LockField(type = str, default = '')
+    type = lock.LockField[ErrorT](type = str, default = 'ERROR')
 # Local Imports
 class ErrorBuffer(lock.LockIO):
-    errors = lock.ListField(lock.SpreadKwargs(Message), default = [])
-    version = lock.LockField(type = str, default = "1.0")
-    def __init__(self, lock : lock.CaliciLock, version : str = "1.0"):
+    errors = lock.ListField(lock.SpreadKwargs(ErrorMessage), default = [])
+    version = lock.LockField(type = str, default = "2.0")
+
+    def __init__(self, lock : lock.CaliciLock, version : str = "2.0"):
         super().__init__(lock.error_path(), version = version)
     # Override saving and loading to json
     def loader(self, f: IO[str]):
@@ -18,9 +24,16 @@ class ErrorBuffer(lock.LockIO):
     def dumper(self, f : IO[str], data : dict):
         return json.dump(data, f)
     # Others
-    def add_entry(self, title : str, content : str, commit : bool = True):
-        self.errors.append({
-            'title' : title, 'content' : content
-        })
+    def add_entry(self, 
+        title : str, content : str, type : ErrorT = "ERROR", 
+        commit : bool = True
+    ):
+        """
+            Adds entry to the error file, 
+            type : "WARNING" or "ERROR"
+        """
+        self.errors.append(
+            { 'title' : title, 'content' : content, 'type' : type }
+        )
         if commit:
             self.save()
