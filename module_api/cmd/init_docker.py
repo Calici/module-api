f425fbba3af1812dbb32c5cb779b14d56dd0c103
Jonathan Willianto <jowillianto@gmail.com>
2023-11-29 11:08:47 +0900

    Fix a bug associated with dockerfile

--- module_api/cmd/init_docker.py
+++ module_api/cmd/init_docker.py
@@ -7,7 +7,7 @@ DOCKER_TEMPLATE = TEMPLATE_DIR / 'dockerfile.tmp'
 class InitDockerHandler(ActionHandler):
     def __init__(self, manifest: pathlib.Path, workdir: pathlib.Path):
         super().__init__(manifest, workdir)
-        self.docker_path = self.root_dir.parent / 'dockerfile'
-        
+        self.docker_path = self.root_dir / 'dockerfile'
+
     def action(self):
         shutil.copyfile(DOCKER_TEMPLATE, self.docker_path)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import \
 
 setup(
     name='module-api',
-    version='0.0.22',    
+    version='0.0.23',    
     description='A Python Package to communicate with the module frontend and backend',
     url='https://github.com/calici/module-api',
     author='Jonathan Willianto',
