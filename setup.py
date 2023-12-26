51a3a71c2934d2d71adefd12be39fb27c4e3cf4e
Jonathan Willianto <jowillianto@gmail.com>
2023-12-26 15:24:28 +0900

    [#6] slowly going to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import \
 
 setup(
     name='module-api',
-    version='0.0.30',    
+    version='0.0.31',    
     description='A Python Package to communicate with the module frontend and backend',
     url='https://github.com/calici/module-api',
     author='Jonathan Willianto',
