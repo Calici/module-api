61a98217df7644b23dae3732d806216bfdca6cd1
Jonathan Willianto <jowillianto@gmail.com>
2024-03-08 13:48:37 +0900

    [#429] Added validation and tools for deployment

--- module_api/cmd/__init__.py
+++ module_api/cmd/__init__.py
@@ -1,34 +1,3 @@
-from .init import \
-    InitDir, \
-    InitDockerHandler, \
-    InitLock, \
-    InitManifestHandler, \
-    InitWorkdirHandler
-from .container import \
-    RunDocker, \
-    BuildDocker, \
-    ContainerName
-from .testing import \
-    TestRefresh, \
-    TestAll, \
-    TestList, \
-    MakeTest, \
-    ResetAllTest
-
-__all__ = [
-    'InitDir', 
-    'InitDockerHandler',
-    'InitLock',
-    'InitManifestHandler',
-    'InitWorkdirHandler',
-
-    'RunDocker', 
-    'BuildDocker', 
-    'ContainerName',
-
-    'TestRefresh', 
-    'TestAll', 
-    'TestList', 
-    'MakeTest', 
-    'ResetAllTest'
-]
+from .init import *
+from .container import *
+from .testing import *
--- module_api/cmd/container/container_name.py
+++ module_api/cmd/container/container_name.py
@@ -5,6 +5,7 @@ from module_api.cmd.base import \
 
 class ContainerName(ActionHandler):
     def __init__(self, lock : ModuleLock):
+        super().__init__(lock)
         self.container = Container(lock)
     def action(self):
         print(self.container.name())
