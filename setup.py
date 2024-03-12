4ef6e9393c7b70fe2c1677659ee1f3ff166fb8f2
Jonathan Willianto <jowillianto@gmail.com>
2024-03-12 23:58:41 +0900

    [#12] Fix indentation

--- setup.py
+++ setup.py
@@ -14,18 +14,18 @@ setup(
         f"module_api.{pkg_name}" for pkg_name in find_namespace_packages("module_api")
     ],
     install_requires=[
-      "annotated-types==0.5.0",
-      "certifi==2023.7.22",
-      "charset-normalizer==3.3.2",
-      "idna==3.4",
-      "pydantic==2.4.2", 
-      "pydantic_core==2.10.1",
-      "pytz==2023.3.post1",
-      "PyYAML==6.0.1",
-      "requests==2.31.0",
-      "typing_extensions==4.7.1",
-      "urllib3==2.0.7", 
-      "pypharmaco @ https://github.com/Calici/pypharmaco/releases/download/v0.0.1/pypharmaco-0.0.1-py3-none-any.whl"
+        "annotated-types==0.5.0",
+        "certifi==2023.7.22",
+        "charset-normalizer==3.3.2",
+        "idna==3.4",
+        "pydantic==2.4.2", 
+        "pydantic_core==2.10.1",
+        "pytz==2023.3.post1",
+        "PyYAML==6.0.1",
+        "requests==2.31.0",
+        "typing_extensions==4.7.1",
+        "urllib3==2.0.7", 
+        "pypharmaco @ https://github.com/Calici/pypharmaco/releases/download/v0.0.1/pypharmaco-0.0.1-py3-none-any.whl"
     ],
     include_package_data = True
 )
